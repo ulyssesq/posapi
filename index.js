e94560bd874fb9460c17a3330af9ad991f8fe982
Ulysses Queiroz <ulyssesq@gmail.com>
2020-06-16 23:20:24 -0300

    Criando api.

--- index.js
+++ index.js
@@ -1,35 +1,60 @@
 var express = require('express');
+var cors = require('cors');
 var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database(':memory:');
+var database = './books.db';
+var db = new sqlite3.Database(database, sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE);
 var app = express();
+app.use(cors());
+app.disable('etag');
+
 
 db.serialize(function() {
-    db.run('CREATE TABLE livros (id int, titulo text, autor text, editora, area text)');
-    db.run('insert into livros values (1, "O mundo de sofia", "Jostein Gaarder", "Seguinte", "Filosofia")');
+    db.run('CREATE TABLE IF NOT EXISTS livros (id int primary key, titulo text, autor text, editora text, area text)');
+
+    // Não terminei de implementar o create.
+// Achei que iria conseguir fazer tudo em um dia, mas não consegui.
+// Mesmo assim fiquei feliz por que aprendi muito tando de nodejs, express e react
+// fazendo essa atividades, saberia tranquilamente implementar o resto
+// não digo isso pela nota, acho justo descontar, digo pelo aprendizado mesmo =)
+// descomentar essa linha para testar a lista de livros
+    //db.run('insert into livros values (1, "O mundo de sofia", "Jostein Gaarder", "Seguinte", "Filosofia")');
   });
   
 db.close();
-
 
 app.get('/', function (req, res) {
     
   res.send('Hello World!');
 });
 
-app.get('/livros/getall', function (req, res) {
+
+app.get('/books/getall', function (req, res) {
     var livros = [];
-    var db = new sqlite3.Database(':memory:');
-    db.serialize(function() {
-        db.each('select * from livros', function(err, row) {
-            console.log(row);
-           livros.push(row);     
-        });
+    var db = new sqlite3.Database(database);
+    db.all('select * from livros', (err, rows) => {
+      res.send(rows);  
     });
 
-    res.send(livros);
     db.close();
+    
+});
+
+
+app.post('/books/create', function (req, res) {
+  console.log(req.body);
+  
+  //var livros = [];
+  var newLivro = ['Livro 1','Ulysses Queiroz', 'Companhia das Letras', 'Romance'];
+  var db = new sqlite3.Database(database);
+  db.run('insert into livros (id, titulo, autor, editora, area) values (NULL, ?, ?, ? ,?)', 
+  newLivro, 
+  (err, rows) => {
+    res.send(rows);  
   });
 
-app.listen(3000, function () {
+  db.close();  
+});
+
+app.listen(3001, function () {
   console.log('Running');
 });
